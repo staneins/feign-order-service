27fb9589a6d1aa26fe127535bd491d0354216aeb
Philip Kaminsky <kaminsky8836@gmail.com>
2025-01-08 11:35:12 +0300

    final commit

--- NotificationsService/src/main/java/com/kaminsky/notificationsservice/controller/NotificationController.java
+++ NotificationsService/src/main/java/com/kaminsky/notificationsservice/controller/NotificationController.java
@@ -1,20 +1,19 @@
 package com.kaminsky.notificationsservice.controller;
 
-import com.kaminsky.feignconfig.entity.OrderRequest;
+import com.kaminsky.feignconfig.entity.NotificationRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController {
     private Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-    public ResponseEntity<String> ceceiveNotification(@RequestBody OrderRequest orderRequest) {
-        log.info("Received order request: {} {} {} {}", orderRequest.getId(), orderRequest.getProductName(), orderRequest.getQuantity(), orderRequest.getUserEmail());
+    @PostMapping
+    public ResponseEntity<String> receiveNotification(@RequestBody NotificationRequest notificationRequest) {
+        log.info("Received order request: {} {} {} {}", notificationRequest.getOrderId(), notificationRequest.getProductName(), notificationRequest.getUserEmail());
         return ResponseEntity.ok().body("Order request received");
     }
 }
